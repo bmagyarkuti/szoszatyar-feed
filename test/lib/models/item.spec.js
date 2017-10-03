20f87b05236a624980fa518c4165182fc47f29c6
Barna Magyarkuti <bmagyarkuti@gmail.com>
2017-10-03 19:17:48 +0200

    fetchOrDownloadSize can fetch size from DB

---
+++ test/lib/models/item.spec.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const Item = require('../../../lib/models/item');
+const mongoWrapper = require('../../../lib/mongoWrapper').create();
+
+describe('Item', function() {
+    before(async function() {
+        await mongoWrapper.connect();
+    })
+    beforeEach(async function() {
+        await mongoWrapper.dropDatabase();
+    });
+
+    describe('.fetchOrDownloadSize', function() {        
+        context('there is already a matching item in DB', function() {
+            const itemInDb = new Item({
+                name: 'http://www.example.com/ex.mp3',
+                size: 111
+            });
+            
+            beforeEach(async function() {
+                await itemInDb.save();
+            });
+            
+            it('fetches size from DB', async function() {
+                expect(await Item.fetchOrDownloadSize(itemInDb.name)).to.eql(itemInDb.size);
+            });            
+        })
+    })
+});
