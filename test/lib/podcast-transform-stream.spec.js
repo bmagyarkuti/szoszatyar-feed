aa9fefd6bdec45637db79d3ed8fd8301a6b452ee
Barna Magyarkuti <bmagyarkuti@gmail.com>
2017-10-23 09:28:30 +0200

    send title as guid

--- test/lib/podcast-transform-stream.spec.js
+++ test/lib/podcast-transform-stream.spec.js
@@ -105,6 +105,10 @@ describe('PodcastTransformStream', function() {
         expect(parsedResult.rss.channel[0].item[0].link[0]).to.eql(item1.link);        
     });
 
+    it('writes guid tag to first item in channel', function() {
+        expect(parsedResult.rss.channel[0].item[0].guid[0]).to.eql(item1.link);        
+    });
+
     it('writes description tag to first item in channel', function() {
         expect(parsedResult.rss.channel[0].item[0].description[0]).to.eql(
             item1.description
