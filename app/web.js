e66d47cef64b718a130c4fd8fdbdc39711ac35c2
Barna Magyarkuti <bmagyarkuti@gmail.com>
2017-11-12 14:23:22 +0100

    sorry, no favicons

--- app/web.js
+++ app/web.js
@@ -2,8 +2,11 @@
 
 const Koa = require('koa');
 const app = new Koa();
+const route = require('koa-route');
+
 const feed = require('./controllers/feed.js');
 
-app.use(feed);
+app.use(route.get("/favicon.ico", ctx => ctx.status = 404));
+app.use(route.get("*", feed));
 
 module.exports = app;
